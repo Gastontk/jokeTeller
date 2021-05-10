ac210a56b11814e6ffbbbefac6c3a3b0415ee877
Gaston Kennedy <gastonkennedy@Gastons-MacBook-Air.local>
2021-05-09 22:17:34 -0700

    added fields to DB collection

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const button = document.getElementById('button');
-// const audioElement = document.getElementById('audio');
 let audioElement = document.createElement('audio');
 const container = document.getElementById('container');
 
@@ -8,10 +7,6 @@ const voicerssKey ='69c03f9d48494f86ba442cb959c2386b';
 let srcText ='four score and seven years ago'
 let setup ='';
 let delivery =''
-
-
-// VoiceRSS Javascript SDK
-// const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
 
 // VoiceRSS Javascript SDK
@@ -70,15 +65,7 @@ const VoiceRSS = {
     container.appendChild(audioElement).play();
     audioElement.controls =true;
     audioElement.hidden= false
-     audioElement.play();
-
-
-                    // console.log('rspones',t.responseText)
-                    // new Audio(t.responseText).play();
-            //    audioElement.src = t.responseText;
-            //    console.log(audioElement)
-            //      audioElement.play();
-            }
+    audioElement.play();            }
         }
         ,
         t.open("POST", "https://api.voicerss.org/", !0),
@@ -147,22 +134,13 @@ async function getJoke(){
     let textToSend =''
     if(!json.joke){
         console.log('double')
-        textToSend = json.setup +'    ' +json.delivery;
-        // setup=json.setup;
-        // delivery =json.delivery;
-        // getVoice(setup +'....'+delivery)
-        
+        textToSend = json.setup +'    ' +json.delivery;        
     }else{
         console.log('single')
         textToSend = json.joke;
-        // joke=json.joke;
-        // getVoice(joke);
     }
     jokeTextElement.textContent = textToSend;
-    getVoice(textToSend);
-
-
-    
+    getVoice(textToSend);   
 };
 
  async function checkIP(){
@@ -173,6 +151,7 @@ async function getJoke(){
             let now = Date.now();
             let timeDate = new Date(now);
             res.timeDate=timeDate;
+            res.app ='joke-teller'
             console.log('checkpoint');
            console.log(res);
                    sendIpToFirebase(res)
@@ -180,6 +159,7 @@ async function getJoke(){
            getJoke()
        });
 }
+
 function sendIpToFirebase(res){
     var firebaseConfig = {
         apiKey: "AIzaSyB61iGOSYQlOCo1rGU0qjc9mYNT9SqNEsM",
@@ -193,7 +173,7 @@ function sendIpToFirebase(res){
     firebase.initializeApp(firebaseConfig);
     let db =firebase.firestore()
     db.collection("ips").add({
-        res
+        ...res
     }).then((docRef)=>{
         // unnimportant. If it fails, it's not important to the function of the app.
     })
@@ -204,5 +184,4 @@ function sendIpToFirebase(res){
     
    
 
-checkIP()
-// getJoke()
+checkIP();
